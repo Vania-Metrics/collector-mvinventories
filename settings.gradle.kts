270ed884099b56c0d144cf46d4dc1cd4d368086a
Thesam1798 <20329547+Thesam1798@users.noreply.github.com>
2026-09-23 17:08:13 +0200

    Rename to collector-*, jars to vania-metrics-*

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@
 // Overrides, for a single build:
 //   ./gradlew build -PvaniaCore.ref=main          a different core ref
 //   ./gradlew build -PvaniaCore.dir=../core       a local core (API dev)
-rootProject.name = "colecteur-mvinventories"
+rootProject.name = "collector-mvinventories"
 
 dependencyResolutionManagement {
     repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
